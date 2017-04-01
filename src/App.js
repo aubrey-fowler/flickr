8b762e38f36ae363b3571a99ac1beb0e50fe87e6
Aubrey Quinn <aubreyfo@gmail.com>
2017-04-01 18:00:44 +0100

    added redux template to project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { connect } from 'react-redux';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        APP
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (store) => {
+    return { 
+        //currentCity: store.currentCity
+    };
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        // setCityData: (id) => {
+        //     dispatch(setCityData(id));
+        // }
+    };
+}
+
+App.propTypes = {
+};
+
+App.contextTypes = {
+    store: React.PropTypes.object.isRequired
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/reducers/reducers.js
@@ -0,0 +1,7 @@
+
+const initialState = {
+};
+
+export function appReducer(state = initialState, action) {
+    return state;
+}
